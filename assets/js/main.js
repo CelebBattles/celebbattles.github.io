9a6ec5e636905c3299c90504e6bb828fb0475789
CelebBattles <saduh777@gmail.com>
2021-08-22 12:58:18 +0530

    celeb pages

--- assets/js/main.js
+++ assets/js/main.js
@@ -143,16 +143,22 @@
 
 })()
 
-// Load images of top celebs when index page loads
+/**
+ * Load images of top celebs when index page loads
+ */
 $(document).ready(function () {
     $.get('../../rankings.json', function (myJson) {
-        console.log(window.location.pathname)
-        console.log(myJson)
+        if (window.location.pathname !== '/') {
+            return;
+        }
+
         var i = 0;
         for (const celeb in myJson) {
             const imgDiv = $('<div/>', { 'class': 'member-img' })
+            const ahref = $('<a/>', { id: 'ahref-r' + i })
             const img = $('<img/>', { id: 'img-r' + i, 'class': 'img-fluid' })
-            imgDiv.append(img)
+            ahref.append(img)
+            imgDiv.append(ahref)
 
             var infoDiv = $('<div/>', { 'class': 'member-info' })
             var h4 = $('<h4/>', { id: 'name-r' + i })
@@ -172,8 +178,58 @@ $(document).ready(function () {
             $('#name-r' + i).html(celeb);
             $('#rank-r' + i).html(`Rank ${myJson[celeb]['rank']}, ELO ${myJson[celeb]['rat']} ${myJson[celeb]['rchange'] === "-" ? "" : myJson[celeb]['rchange']}`);
             $('#img-r' + i).attr('src', `assets/img/${celeb}.jpg`);
+            $('#ahref-r' + i).attr('href', `https://celebbattles.github.io/details?name=${celeb}`)
 
             i++;
+
+            if (i == 32) {
+                break;
+            }
         }
     });
 });
+
+/**
+ * Load celeb information when details page loads
+ */
+$(document).ready(function () {
+    if (window.location.pathname !== '/details') {
+        return;
+    }
+    const hashes = getUrlVars();
+    const celeb = hashes.name.replaceAll('%20', ' ')
+
+    $.get('../../rankings.json', function (myJson) {
+        const freq = myJson[celeb]['freq'].split(",")
+        const freq_str = `${freq[0]} <br> ${freq[1]} battles, ${Number(freq[1]) - Number(freq[2])} wins (${Math.round((Number(freq[1]) - Number(freq[2])) / Number(freq[1]) * 100 * 10) / 10}%)`
+        const best = myJson[celeb]['best'].split(",")
+        const best_str = `${best[0]} <br> ${best[1]} battles, ${Number(best[1]) - Number(best[2])} wins (${Math.round((Number(best[1]) - Number(best[2])) / Number(best[1]) * 100 * 10) / 10}%)`
+        const worst = myJson[celeb]['worst'].split(",")
+        const worst_str = `${worst[0]} <br> ${worst[1]} battles, ${Number(worst[1]) - Number(worst[2])} wins (${Math.round((Number(worst[1]) - Number(worst[2])) / Number(worst[1]) * 100 * 10) / 10}%)`
+
+        $('#celeb-name').html(celeb)
+        $('#celeb-img').attr('src', `assets/img/${celeb}.jpg`);
+        $('#rank').html(myJson[celeb]['rank'])
+        $('#h-rank').html(myJson[celeb]['hrank'])
+        $('#rat').html(myJson[celeb]['rat'])
+        $('#dev').html(myJson[celeb]['dev'])
+        $('#num').html(myJson[celeb]['num'])
+        $('#oppo').html(myJson[celeb]['opp'])
+        $('#winPer').html(`${myJson[celeb]['winPer']}%`)
+        $('#freq-oppo').html(freq_str)
+        $('#best-score').html(best_str)
+        $('#worst-score').html(worst_str)
+    });
+});
+
+// Read a page's GET URL variables and return them as an associative array.
+function getUrlVars() {
+    var vars = [], hash;
+    var hashes = window.location.href.slice(window.location.href.indexOf('?') + 1).split('&');
+    for (var i = 0; i < hashes.length; i++) {
+        hash = hashes[i].split('=');
+        vars.push(hash[0]);
+        vars[hash[0]] = hash[1];
+    }
+    return vars;
+}
